72f582c96796b95f2ef0d60f2af5bc3f841f71bf
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-10-27 18:20:59 +0200

    done

--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ $(function(){
 
     $('#spentAmount').click(function(){
         
-        chrome.storage.sync.get('total',function(budget){
+        chrome.storage.sync.get(['total', 'limit'],function(budget){
             var newTotal = 0;
             if(budget.total){
                 newTotal += parseInt(budget.total);
@@ -20,7 +20,18 @@ $(function(){
                 newTotal += parseInt(amount);
             }
 
-            chrome.storage.sync.set({'total': newTotal});
+            chrome.storage.sync.set({'total': newTotal}, function(){               
+                if (amount && newTotal >= budget.limit){
+                    var notifOptions = {
+                        type: "basic",
+                        iconUrl: "icon48.png",
+                        title: "Limit reached!",
+                        message: "Uh oh, look's like you've reached your alloted limit."
+                };
+                chrome.notifications.create('limitNotif', notifOptions);
+                
+                }
+            });
 
             $('#total').text(newTotal);
 
